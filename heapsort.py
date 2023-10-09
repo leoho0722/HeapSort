985bc990bf7aeb15de8813342b60967195ded6e0
leoho0722 <leo160918@gmail.com>
2023-10-09 13:05:35 -0500

    Update Code Commits of Python Example

--- heapsort.py
+++ heapsort.py
@@ -33,13 +33,16 @@ def heapsort(ary):
     for start in range((len(ary) - 2)):
         siftdown(start, len(ary) - 1)
 
-    # Heap Sort
+    # 針對未排序的部分進行排序，並透過迭代將 Max Heap 轉換成 Min Heap
     for end in range(len(ary) - 1, 0, -1):
+        # 將 Heap 的根節點與陣列的最後一個元素交換
         ary[0], ary[end] = ary[end], ary[0]
+
+        # 重新調整 Heap 並縮小範圍
+        # Heap 內不包含先前已排序好的元素 (最大元素會放到陣列最後面)
         siftdown(0, end - 1)
 
     return ary
-
 
 if __name__ == "__main__":
     ary = [55, 94, 87, 1, 4, 32, 11]
